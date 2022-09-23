d5724ee509e48fd4a5c3d4b87c129b3cb56dd94d
Limhyeonsu <dlagustn720@naver.com>
2022-09-23 18:48:32 +0900

    스프링 핵심 원리 이해 2 - 객체지향 원리 적용 (SRP, DIP, OCP)

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,32 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+//객체의 생성과 연결을 담당한다.
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository () {
+        return new MemoryMemberRepository();
+    }
+
+    //AppConfig에서 구성 역할을 담당하기 때문에 구현 객체를 변경하면 사용 영역의 어떤 코드도 변경할 필요가 없다.
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
