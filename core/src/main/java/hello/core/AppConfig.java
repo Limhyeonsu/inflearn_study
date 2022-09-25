e127431daac06221ae5e15c87082f9a3ad936bbb
Limhyeonsu <dlagustn720@naver.com>
2022-09-25 23:51:35 +0900

    스프링 핵심 원리 이해 2 - IoC, DI, 컨테이너, 스프링으로 전환(1)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //객체의 생성과 연결을 담당한다.
+@Configuration
 public class AppConfig {
+    //@Bean = 스프링 컨테이너에 스프링 빈으로 등록해 준다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository () {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     //AppConfig에서 구성 역할을 담당하기 때문에 구현 객체를 변경하면 사용 영역의 어떤 코드도 변경할 필요가 없다.
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
