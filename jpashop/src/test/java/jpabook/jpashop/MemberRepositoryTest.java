8957c1353e87a0b2af12020d04e872e676e08ff8
Limhyeonsu <dlagustn720@naver.com>
2023-02-05 19:27:54 +0900

    JPA 1편 - JPA와 DB설정, 동작확인

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember () {
+        Member member = new Member();
+        member.setUsername("mamberA");
+        Long saveId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        //같은 영속 컨텍스트안에 있으므로 같은 객체를 반환한다.
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+}
