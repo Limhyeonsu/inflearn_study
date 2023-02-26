f061ca774a85edc147d4db4d00cd20cecae6c1e9
Limhyeonsu <dlagustn720@naver.com>
2023-02-26 23:07:49 +0900

    JPA 1편 - 상품 도메인 개발, 주문 도메인 개발(주문, 주문상품 엔티티 개발)

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -21,6 +21,28 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order; //주문
-    private int orderPrice; //주문 가격 private int count; //주문 수량
+    private int orderPrice; //주문 가격
+    private int count; //주문 수량
 
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        //주문시 item의 재고를 수정해야 한다.
+        item.removeStock(count);
+        return orderItem;
+    }
+    //비즈니스 로직
+    public void cancel() {
+        //Item의 재고 수량을 원복시켜준다.
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    public int getTotalPrice() {
+        //주문 가격 * 주문 수량
+        return getOrderPrice() * getCount();
+    }
 }
