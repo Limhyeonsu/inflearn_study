cf72d7f899e32e924b60c008ffb9531ee4ff0907
Limhyeonsu <dlagustn720@naver.com>
2023-04-24 23:54:22 +0900

    JPA 2편 - 스프링 데이터 JPA, Query dsl

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -33,4 +33,6 @@ public class MemberRepository {
                 .setParameter("name", name)
                 .getResultList();
     }
+
+
 }
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,9 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
 
 @Repository
 public class OrderRepository {
@@ -130,4 +135,37 @@ public class OrderRepository {
                 .setMaxResults(limit)
                 .getResultList();
     }
+
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+
+    private BooleanExpression statusEq(OrderStatus status) {
+        if(status == null)
+            return null;
+
+        return order.status.eq(status);
+
+    }
+
+    private BooleanExpression nameLike(String name) {
+        if(name == null)
+            return null;
+
+        return member.name.like(name);
+
+    }
+
 }
